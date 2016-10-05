01d1994650cc96f8e1e2a7f4c4d92a6be90b9b59
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-10-05 17:39:37 +0200

    Adding code for Stand method based on sequence diagram

--- workshop-3/grade-2/BlackJack/model/Dealer.java
+++ workshop-3/grade-2/BlackJack/model/Dealer.java
@@ -57,5 +57,21 @@ public class Dealer extends Player {
     }
     return false;
   }
-  
+
+  public boolean Stand() {
+    if (m_deck != null) {
+      ShowHand();
+
+      while (m_hitRule.DoHit(this)) {
+        Card c = m_deck.GetCard();
+        c.Show(true);
+        DealCard(c);
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+
 }
