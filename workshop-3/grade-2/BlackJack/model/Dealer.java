3bfd4ef691f66dee6dc6ba3addd8fa47e63daa45
Simon Palmqvist <simon.palmqvist@gmail.com>
2016-10-05 18:54:01 +0200

    Removing duplication in dealing cards

--- workshop-3/grade-2/BlackJack/model/Dealer.java
+++ workshop-3/grade-2/BlackJack/model/Dealer.java
@@ -9,33 +9,23 @@ public class Dealer extends Player {
   private IHitStrategy m_hitRule;
 
   public Dealer(RulesFactory a_rulesFactory) {
-  
     m_newGameRule = a_rulesFactory.GetNewGameRule();
     m_hitRule = a_rulesFactory.GetHitRule();
-    
-    /*for(Card c : m_deck.GetCards()) {
-      c.Show(true);
-      System.out.println("" + c.GetValue() + " of " + c.GetColor());
-    }    */
   }
-  
   
   public boolean NewGame(Player a_player) {
     if (m_deck == null || IsGameOver()) {
       m_deck = new Deck();
       ClearHand();
       a_player.ClearHand();
-      return m_newGameRule.NewGame(m_deck, this, a_player);   
+      return m_newGameRule.NewGame(this, a_player);
     }
     return false;
   }
 
   public boolean Hit(Player a_player) {
     if (m_deck != null && a_player.CalcScore() < g_maxScore && !IsGameOver()) {
-      Card c;
-      c = m_deck.GetCard();
-      c.Show(true);
-      a_player.DealCard(c);
+      GetNewCardAndDealTo(a_player);
       
       return true;
     }
@@ -52,7 +42,7 @@ public class Dealer extends Player {
   }
 
   public boolean IsGameOver() {
-    if (m_deck != null && m_hitRule.DoHit(this) != true) {
+    if (m_deck != null && !m_hitRule.DoHit(this)) {
         return true;
     }
     return false;
@@ -61,17 +51,23 @@ public class Dealer extends Player {
   public boolean Stand() {
     if (m_deck != null) {
       ShowHand();
-
       while (m_hitRule.DoHit(this)) {
-        Card c = m_deck.GetCard();
-        c.Show(true);
-        DealCard(c);
+        GetNewCardAndDealTo(this);
       }
-
       return true;
     }
 
     return false;
+  }
+
+  public void GetNewCardAndDealTo(Player a_player) {
+    GetNewCardAndDealTo(a_player, true);
+  }
+
+  public void GetNewCardAndDealTo(Player a_player, boolean show) {
+    Card c = m_deck.GetCard();
+    c.Show(show);
+    a_player.DealCard(c);
   }
 
 }
--- workshop-3/grade-2/BlackJack/model/rules/AmericanNewGameStrategy.java
+++ workshop-3/grade-2/BlackJack/model/rules/AmericanNewGameStrategy.java
@@ -1,30 +1,15 @@
 package BlackJack.model.rules;
 
-import BlackJack.model.Deck;
 import BlackJack.model.Dealer;
 import BlackJack.model.Player;
-import BlackJack.model.Card;  
 
 class AmericanNewGameStrategy implements INewGameStrategy {
 
-  public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
-    Card c;
-
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_player.DealCard(c);
-
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_dealer.DealCard(c);
-
-    c = a_deck.GetCard();
-    c.Show(true);
-    a_player.DealCard(c);
-
-    c = a_deck.GetCard();
-    c.Show(false);
-    a_dealer.DealCard(c);
+  public boolean NewGame(Dealer a_dealer, Player a_player) {
+    a_dealer.GetNewCardAndDealTo(a_player);
+    a_dealer.GetNewCardAndDealTo(a_dealer);
+    a_dealer.GetNewCardAndDealTo(a_player);
+    a_dealer.GetNewCardAndDealTo(a_dealer, false);
 
     return true;
   }
